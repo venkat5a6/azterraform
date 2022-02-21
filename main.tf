3875f1c2dd85573095ae78b80953c29913914527
venkat5a6 <99792584+venkat5a6@users.noreply.github.com>
2022-02-21 17:06:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  client_id = "39359ac3-9b2c-4cac-96df-ec246b0642b1"
-  client_secret = "LzV2wIxf3vPZFCF5a-.~NwN-PP7.5nmV0p"
-  tenant_id = "e9ba9f78-36bf-4339-9cc3-cd4adbbf19b3"
+  "client_id": "f1744cce-acd0-4f01-8d72-f4d0e53b1acd"
+  "client_secret": "dh0~k5sKzUeN4PJXDAlPb9sCU~7KfgtXTJ"
+  "tenant_id": "e355c7e8-852d-4e18-8f92-9623db3b5291"
   subscription_id = "4fa1aa2a-bd64-49e7-a3cf-65127add0baa"
 }
 # Create a resource group if it doesn't exist
@@ -26,7 +26,7 @@ resource "azurerm_virtual_network" "alpha" {
     name                = "alphaVnet"
     address_space       = ["10.0.0.0/16"]
     location            = "eastus2"
-    resource_group_name = "ssadcloud-RG"
+    resource_group_name = "azrg1"
     tags = {
         environment = "Demo"
     }
@@ -34,7 +34,7 @@ resource "azurerm_virtual_network" "alpha" {
 # Create subnet
 resource "azurerm_subnet" "alpha" {
     name                 = "alphaSubnet"
-    resource_group_name  = "ssadcloud-RG"
+    resource_group_name  = "azrg1"
     virtual_network_name = azurerm_virtual_network.alpha.name
     address_prefixes     = ["10.0.1.0/24"]
 }
@@ -42,7 +42,7 @@ resource "azurerm_subnet" "alpha" {
 resource "azurerm_public_ip" "alpha" {
     name                         = "alphaip"
     location                     = "eastus2"
-    resource_group_name          = "ssadcloud-RG"
+    resource_group_name          = "azrg1"
     allocation_method            = "Static"
     domain_name_label            = random_string.fqdn.result
     tags = {
@@ -52,7 +52,7 @@ resource "azurerm_public_ip" "alpha" {
 resource "azurerm_lb" "alpha" {
   name                = "alphaALB"
   location            = "eastus2"
-  resource_group_name ="ssadcloud-RG"
+  resource_group_name ="azrg1"
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
     public_ip_address_id = azurerm_public_ip.alpha.id
@@ -63,13 +63,13 @@ resource "azurerm_lb_backend_address_pool" "bpepool" {
   name                = "BackEndAddressPool"
 }
 resource "azurerm_lb_probe" "alpha" {
-  resource_group_name ="ssadcloud-RG"
+  resource_group_name ="azrg1"
   loadbalancer_id     = azurerm_lb.alpha.id
   name                = "http-probe"
   port                = 8080
 }
 #resource "azurerm_lb_nat_pool" "alpha" {
-#  resource_group_name            = "ssadcloud-RG"
+#  resource_group_name            = "azrg1"
 #  loadbalancer_id                = azurerm_lb.alpha.id
 #  name                           = "ssh"
 #  protocol                       = "Tcp"
@@ -78,7 +78,7 @@ resource "azurerm_lb_probe" "alpha" {
 #  frontend_ip_configuration_name = "PublicIPAddress"
 #}
 resource "azurerm_lb_rule" "alpha" {
-  resource_group_name            = "ssadcloud-RG"
+  resource_group_name            = "azrg1"
   loadbalancer_id                = azurerm_lb.alpha.id
   name                           = "LBRule"
   protocol                       = "Tcp"
@@ -88,13 +88,13 @@ resource "azurerm_lb_rule" "alpha" {
   frontend_ip_configuration_name = "PublicIPAddress"
 }
 data "azurerm_image" "iris-az-packer-image" {
-  resource_group_name = "ssadcloud-RG"
+  resource_group_name = "azrg1"
   name                = "iris-az-packer-image"
 }
 resource "azurerm_virtual_machine_scale_set" "alpha" {
 name                = "alpha-vmss"
 location            = "eastus2"
-resource_group_name ="ssadcloud-RG"
+resource_group_name ="azrg1"
 # automatic rolling upgrade
 automatic_os_upgrade = false
 upgrade_policy_mode  = "Manual"
